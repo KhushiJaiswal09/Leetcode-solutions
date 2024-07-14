ebe5e54c22ba9919705ddbf57de2cecfc008e926
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-07-14 20:47:07 +0530

    Time: 134 ms (77.31%) | Memory: 41.4 MB (67.54%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,28 +1,47 @@
 class Solution {
-  public boolean exist(char[][] board, String word) {
-    for (int i = 0; i < board.length; ++i)
-      for (int j = 0; j < board[0].length; ++j)
-        if (dfs(board, word, i, j, 0))
-          return true;
+    public boolean exist(char[][] board, String word) {
+
+       
+        
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+
+                if(word.charAt(0)==board[i][j]){
+                    boolean result=ans(board, word, i, j, 0);
+                    if(result) return true;
+                }
+            }
+        }
+    
     return false;
-  }
+        
+    }
+    public boolean ans(char[][] board, String word, int row, int col, int index){
+        
+        if(word.length()==index){
+            return true;
+        }
+        
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length){
+            return false;
+        }
 
-  private boolean dfs(char[][] board, String word, int i, int j, int s) {
-    if (i < 0 || i == board.length || j < 0 || j == board[0].length)
-      return false;
-    if (board[i][j] != word.charAt(s) || board[i][j] == '*')
-      return false;
-    if (s == word.length() - 1)
-      return true;
+        if(board[row][col]!=word.charAt(index) || board[row][col]==' '){
+            return false;
+        }
 
-    final char cache = board[i][j];
-    board[i][j] = '*';
-    final boolean isExist = dfs(board, word, i + 1, j, s + 1) || //
-                            dfs(board, word, i - 1, j, s + 1) || //
-                            dfs(board, word, i, j + 1, s + 1) || //
-                            dfs(board, word, i, j - 1, s + 1);
-    board[i][j] = cache;
+        char temp=board[row][col];
+        board[row][col]=' ';
 
-    return isExist;
-  }
+        if(ans(board, word, row + 1, col, index + 1) ||
+           ans(board, word, row - 1, col, index + 1) ||
+           ans(board, word, row, col + 1, index + 1) ||
+           ans(board, word, row, col - 1, index + 1)   ){
+
+            return true;
+        }
+
+        board[row][col]=temp;
+        return false;
+    }
 }
