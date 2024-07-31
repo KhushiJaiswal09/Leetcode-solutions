0371bb0bd34515141e280b1cfe50261db5a088c3
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-07-31 22:12:59 +0530

    Time: 22 ms (36.56%) | Memory: 44.6 MB (52.35%) - LeetSync

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -7,14 +7,12 @@ class Solution {
         for(int i=0; i<s.length(); i++){
 
             if(i>=k){
-             if(s.charAt(i-k)=='a' || s.charAt(i-k)=='e' || s.charAt(i-k)=='i' || s.charAt(i-k)=='o' || s.charAt(i-k)=='u' ||
-                s.charAt(i-k)=='A' || s.charAt(i-k)=='E' || s.charAt(i-k)=='I' || s.charAt(i-k)=='O' || s.charAt(i-k)=='U'){
+             if(s.charAt(i-k)=='a' || s.charAt(i-k)=='e' || s.charAt(i-k)=='i' || s.charAt(i-k)=='o' || s.charAt(i-k)=='u'){
                     count=count-1;
                }               
             }
 
-            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' ||
-               s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+            if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u'){
                 count++;
                     }
           
