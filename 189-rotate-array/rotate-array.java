f6bfac05bf95425f7b6e1f8576ecab7391e61bab
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-09-20 11:32:44 +0530

    Time: 0 ms (100.00%) | Memory: 57.4 MB (49.74%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -2,32 +2,23 @@ class Solution {
     public void rotate(int[] nums, int k) {
         
         int n=nums.length;     
-        int i=0, j=n-1;
+        
         k=(k>n)?k%n:k;
         
-        while (j>i){
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
-
-        i=0; j=k-1;
-        int x=k, y=n-1;
-        while (i<j){
-            swap(nums, i, j);
-            i++;
-            j--;
-        }
-        while (x<y){
-            swap(nums, x, y);
-            x++;
-            y--;
-        }
+    
+        swap(nums, 0, n-1);
+        swap(nums, 0, k-1);
+        swap(nums, k, n-1);
+   
     }
 
     static void swap(int[] nums, int num1, int num2){
+        while(num1<num2){
         int temp=nums[num1];
         nums[num1]=nums[num2];
         nums[num2]=temp;
+        num1++;
+        num2--;
+        }
     }
 }
