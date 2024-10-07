04c8d9a031b732422c4a6d7be69cf48df691e517
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2024-10-07 22:56:07 +0530

    Time: 4 ms (39.42%) | Memory: 42.6 MB (6.85%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,37 +1,34 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        
-        List<String> ans=new ArrayList<>();
-         Map<Character, String> map=getmap();
 
-        if(digits.length()==0){
+        List<String> ans = new ArrayList<>();
+        Map<Character, String> map = getmap();
+
+        if (digits.length() == 0) {
             return ans;
         }
-       
 
         solve(0, digits, "", ans, map);
 
         return ans;
     }
 
-    private void solve(int i, String digits, String s, List<String> ans, Map<Character, String> map)
-    {
-        if(i==digits.length()){
+    private void solve(int i, String digits, String s, List<String> ans, Map<Character, String> map) {
+        if (i == digits.length()) {
             ans.add(s);
             return;
         }
 
-        for(char c: map.get(digits.charAt(i)).toCharArray())
-        {
-            String temp=new String(s);
-            temp+=c;
-            solve(i+1, digits, temp, ans, map);
+        for (char c : map.get(digits.charAt(i)).toCharArray()) {
+            String temp = new String(s);
+            temp += c;
+            solve(i + 1, digits, temp, ans, map);
         }
     }
 
-    private Map<Character, String> getmap(){
+    private Map<Character, String> getmap() {
 
-        Map<Character, String> map=new HashMap<>();
+        Map<Character, String> map = new HashMap<>();
         map.put('2', "abc");
         map.put('3', "def");
         map.put('4', "ghi");
