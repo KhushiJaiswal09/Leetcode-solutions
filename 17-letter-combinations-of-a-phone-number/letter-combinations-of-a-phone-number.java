97528f2a87b9b2ab9000b4ba4baf0827fe15fff5
Khushi Jaiswal <103581045+KhushiJaiswal09@users.noreply.github.com>
2025-01-18 23:50:26 +0530

    Time: 6 ms (7.75%) | Memory: 42.5 MB (15.96%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -1,42 +1,35 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-
-        List<String> ans = new ArrayList<>();
-        Map<Character, String> map = getmap();
-
-        if (digits.length() == 0) {
-            return ans;
-        }
-
-        solve(0, digits, "", ans, map);
-
-        return ans;
+        List<String> result=new ArrayList<>();
+        HashMap<Character, String> map=board();
+        if(digits.length()==0) return result;
+        combinations(map, digits, 0, "", result);
+        return result;
     }
-
-    private void solve(int i, String digits, String s, List<String> ans, Map<Character, String> map) {
-        if (i == digits.length()) {
-            ans.add(s);
+    private void combinations(HashMap<Character, String> map, String digits, int i, String p, List<String> result){
+        if(i==digits.length()){
+            result.add(p);
             return;
         }
+        for( char c : map.get(digits.charAt(i)).toCharArray() ){
+            String s=new String(p);
+            s+=c;
+            combinations(map, digits, i+1, s, result);
 
-        for (char c : map.get(digits.charAt(i)).toCharArray()) {
-            String temp = new String(s);
-            temp += c;
-            solve(i + 1, digits, temp, ans, map);
         }
+    
     }
 
-    private Map<Character, String> getmap() {
-
-        Map<Character, String> map = new HashMap<>();
-        map.put('2', "abc");
-        map.put('3', "def");
-        map.put('4', "ghi");
-        map.put('5', "jkl");
-        map.put('6', "mno");
-        map.put('7', "pqrs");
-        map.put('8', "tuv");
-        map.put('9', "wxyz");
+    private HashMap<Character, String> board(){
+        HashMap<Character, String> map=new HashMap<>();
+        map.put('2',"abc");
+        map.put('3',"def");
+        map.put('4',"ghi");
+        map.put('5',"jkl");
+        map.put('6',"mno");
+        map.put('7',"pqrs");
+        map.put('8',"tuv");
+        map.put('9',"wxyz");
 
         return map;
     }
